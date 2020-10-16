b5b3ebbf7a06b3c4b1032e96146c7c1f5d0680f6
ThiagoFrederico <thiagofrederico3@gmail.com>
2020-10-16 10:08:19 -0300

    Criando mapa e e galeria de imagens

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,27 @@
+//Create map
+const map = L.map('mapid').setView([-23.5840863,-46.6607062], 15);
+
+//Create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//Create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight:240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>')
+
+
+//create and add marker
+L.marker([-23.5840863,-46.6607062], {icon}).addTo(map)
+.bindPopup(popup)
